2840515d4cc2f5b0cbd705ec6f92c80fc1485fd6
june850713@gmail.com <june850713@gmail.com>
2022-07-02 12:48:22 +0800

    feat: modify login route via passport

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 const app = express()
 const PORT = 3000
+
+usePassport(app)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
